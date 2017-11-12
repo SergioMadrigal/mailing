9fd4042b46e14d1d133d5d55fe740041ef5def92
Sergio Madrigal <sergio.madrigal_smh@hotmail.com>
2017-11-11 21:45:54 -0600

    first commit

---
+++ build-an-html-email-template-from-scratch-master/build-an-html-email-template-from-scratch-master/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');    
+});
+
+app.use('images', express.static(__dirname + '/images'))
+
+app.listen(3000);
+
+console.log("Run at port 3000");
